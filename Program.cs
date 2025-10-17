82258f5dd9ae50b12d5d8283bff4269d6fc28764
Rogerio de Jesus da Silva <rogeriojesussilva@gmail.com>
2025-10-16 21:16:34 -0300

    Resolve desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia("123456789", "Nokia 3310", "001122334455667", 16);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Smartphone iphone = new Iphone("987654321", "iPhone 12", "765432109876543", 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
